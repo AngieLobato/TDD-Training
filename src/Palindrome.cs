72978c7ecf24d39071248a166b93fd05197ba64c
AngieLobato <angiielobato@gmail.com>
2015-09-23 16:25:03 -0500

    Palindrome tests

--- src/Palindrome.cs
+++ src/Palindrome.cs
@@ -1,6 +1,27 @@
 ﻿namespace CSharp.Training
 {
-    public static class Palindrome
+    public class Palindrome
     {
+        public bool IsPalendrome(string text)
+        {
+            int min = 0;
+            int max = text.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = text[min];
+                char b = text[max];
+
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
     }
 }
--- tests/PalindromeTest.cs
+++ tests/PalindromeTest.cs
@@ -1,6 +1,29 @@
-﻿namespace CSharp.Training.Tests
+﻿using NUnit.Framework;
+
+namespace CSharp.Training.Tests
 {
+    [TestFixture]
     public class PalindromeTest
     {
+        Palindrome Obj = new Palindrome();
+
+        [TestCase("In girum imus nocte et consumimur igni")]
+        [TestCase("Roma tibi subito motibus ibit amor")]
+        [TestCase("civic")]
+        public void TruePalenDrome(string text)
+        {
+            text = text.Replace(" ", "");
+            Assert.AreEqual(Obj.IsPalendrome(text), true);
+        }
+
+        [TestCase("mensaje")]
+        [TestCase("this is a example")]
+        public void FalsePalenDrome(string text)
+        {
+            text = text.Replace(" ", "");
+            Assert.AreEqual(Obj.IsPalendrome(text), false, "FAIL --> " + text);
+        }
+
+        
     }
 }
