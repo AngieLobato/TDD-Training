d2e5bb8ba4c4761abced2411b96818072f547a29
AngieLobato <angiielobato@gmail.com>
2015-09-23 11:45:20 -0500

    Fibonacci positive

--- src/Fibonacci.cs
+++ src/Fibonacci.cs
@@ -2,5 +2,24 @@
 {
     public class Fibonacci
     {
+        public int[] Sequence(int size)
+        {
+            int[] sequence = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i < 2)
+                {
+                    sequence[i] = i;
+                }
+                else
+                {
+                    sequence[i] = sequence[i - 1] + sequence[i - 2];
+                }
+            }
+            return sequence;
+        }
+
+
     }
 }
--- tests/Fibonacci.Tests.cs
+++ tests/Fibonacci.Tests.cs
@@ -6,27 +6,31 @@ namespace CSharp.Training.Tests
     [TestFixture]
     public class FibonacciTests
     {
-       
-    }
-
-    public class Fibonacci
-    {
-        public int[] Sequence(int size)
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        [TestCase(7, 13)]
+        [TestCase(8, 21)]
+        [TestCase(9, 34)]
+        [TestCase(11, 89)]
+        [TestCase(12, 144)]
+        [TestCase(13, 233)]
+        [TestCase(14, 377)]
+        [TestCase(15, 610)]
+        public void FibanocciSequencetest(int index, int value)
         {
-            int[] sequence = new int[size];
+            Assert.AreEqual(value, ValueFibanocci(index));
+        }
 
-            for (int i = 0; i < size; i++)
-            {
-                if (i < 2)
-                {
-                    sequence[i] = i;
-                }
-                else
-                {
-                    sequence[i] = sequence[i - 1] + sequence[i - 2];
-                }
-            }
-            return sequence;
+        private int ValueFibanocci(int index)
+        {
+            return new Fibonacci().Sequence(index + 1)[index];
         }
     }
+
+   
 }
