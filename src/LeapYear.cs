9888b2b4e12966c8fc634b303ca69a4986323e30
AngieLobato <angiielobato@gmail.com>
2015-09-23 12:36:16 -0500

    Tests LeapYear - Create an extension method for Integer type that will determine whether a given year is a leap year in the Gregorian calendar.

--- src/LeapYear.cs
+++ src/LeapYear.cs
@@ -1,6 +1,24 @@
-﻿namespace CSharp.Training
+﻿using System;
+using System.Globalization;
+
+namespace CSharp.Training
 {
     public class LeapYear
     {
+        public string LeapYearCalendar(int anio)
+        {
+            string result = String.Empty; ;
+            GregorianCalendar calendar = new GregorianCalendar();
+
+            if (calendar.IsLeapYear((anio)))
+            {
+                result = "LeapYear";
+            }
+            else
+            {
+                result = "NoLeapYear";
+            }
+            return result;
+        }
     }
 }
--- tests/LeapYear.Test.cs
+++ tests/LeapYear.Test.cs
@@ -1,6 +1,50 @@
-﻿namespace CSharp.Training.Tests
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace CSharp.Training.Tests
 {
-    public class LeapYear
+    public class KnowLeapYear
     {
+        LeapYear IsLeapYear = new LeapYear();
+
+        [TestCase(1997)]
+        [TestCase(1998)]
+        [TestCase(1999)]
+        [TestCase(2001)]
+        [TestCase(2002)]
+        [TestCase(2003)]
+        [TestCase(2005)]
+        [TestCase(2006)]
+        [TestCase(2007)]
+        [TestCase(2009)]
+        [TestCase(2010)]
+        [TestCase(2011)]
+        [TestCase(2013)]
+        [TestCase(2014)]
+        [TestCase(2015)]
+        [TestCase(2017)]
+        [TestCase(2018)]
+        [TestCase(2019)]
+        [TestCase(2021)]
+        public void NotLeapYear(int anio)
+        {
+            Assert.AreEqual(IsLeapYear.LeapYearCalendar(anio), "NoLeapYear", "Incorrect" + anio);
+        }
+
+        [TestCase(1996)]
+        [TestCase(2000)]
+        [TestCase(2004)]
+        [TestCase(2008)]
+        [TestCase(2016)]
+        [TestCase(2012)]
+        [TestCase(2020)]
+        public void YeaLeapYear(int anio)
+        {
+            Assert.AreEqual(IsLeapYear.LeapYearCalendar(anio), "LeapYear", "Incorrect" + anio);
+        }
+
+
+        
     }
 }
