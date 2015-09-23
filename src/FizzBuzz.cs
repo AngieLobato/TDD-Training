6564050d6a7dcd6c41961c5e6a99eab8320162f4
AngieLobato <angiielobato@gmail.com>
2015-09-23 10:17:39 -0500

    All test correct FizzBuzz

--- src/FizzBuzz.cs
+++ src/FizzBuzz.cs
@@ -1,20 +1,16 @@
-﻿using System;
-
+﻿
 namespace CSharp.Training
 {
     public class FizzBuzz
     {
-        /// <summary>
-        /// FizzBuzz Compute Method
-        /// * If the number is divisible by three then the result will be "Fizz"
-        /// * If the number is divisible by five then the result will be "Buzz"
-        /// * If the number is divisible by three and five then the result will be "FizzBuzz"
-        /// </summary>
-        /// <param name="number">Number to compute</param>
-        /// <returns>Computed result</returns>
-        public string Compute(int number)
+        public string Metodo(int num)
         {
-            throw new NotImplementedException("The trainee should implement this code");
+            var x = "Fizz";
+            if (num % 3 == 0) { x = "Fizz"; }
+            if (num % 5 == 0 && num % 3 != 0) { x = "Buzz"; }
+            if (num % 5 == 0 && num % 3 == 0) { x += "Buzz"; }
+            if (num % 5 != 0 && num % 3 != 0) { x = "NoMultiple"; }
+            return x;
         }
     }
 }
--- tests/FizzBuzz.Test.cs
+++ tests/FizzBuzz.Test.cs
@@ -1,6 +1,43 @@
-﻿namespace CSharp.Training.Tests
+﻿using NUnit.Framework;
+
+namespace CSharp.Training.Tests
 {
+    [TestFixture]
     public class FizzBuzzTests
     {
+        FizzBuzz FizzBuzz = new FizzBuzz();
+
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void Multiples3(int num)
+        {
+            Assert.AreEqual(FizzBuzz.Metodo(num), "Fizz", "Incorrect" + num);
+        }
+
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void Multiples5(int num)
+        {
+            Assert.AreEqual(FizzBuzz.Metodo(num), "Buzz", "Incorrect" + num);
+        }
+
+
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(45)]
+        public void MultiplesBoth(int num)
+        {
+            Assert.AreEqual(FizzBuzz.Metodo(num), "FizzBuzz", "Incorrect" + num);
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        public void NoMultiples(int num)
+        {
+            Assert.AreEqual(FizzBuzz.Metodo(num), "NoMultiple", "Incorrect" + num);
+        }
     }
 }
