ca2dcf78a2847955a3cac7d2c68ad978f9585c79
AngieLobato <angiielobato@gmail.com>
2015-09-23 15:31:00 -0500

    perfect number

--- src/PerfectNumber.cs
+++ src/PerfectNumber.cs
@@ -1,7 +1,18 @@
-﻿namespace CSharp.Training
+﻿using System.Runtime.InteropServices;
+
+namespace CSharp.Training
 {
     public class PerfectNumberExtention
     {
-        
+        public bool NumbersTest(int num)
+        {
+            var sum = 0;
+            for (var i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    sum += i;
+            }
+            return sum == num;
+        }
     }
 }
--- tests/PerfectNumberTest.cs
+++ tests/PerfectNumberTest.cs
@@ -1,6 +1,30 @@
-﻿namespace CSharp.Training.Tests
+﻿using NUnit.Framework;
+
+namespace CSharp.Training.Tests
 {
+    [TestFixture]
     public class PerfectNumberTest
     {
+        PerfectNumberExtention Number = new PerfectNumberExtention();
+
+        [Test]
+        [TestCase(6)]
+        [TestCase(28)]
+        [TestCase(496)]
+        [TestCase(8128)]
+        public void IsPerfect(int x)
+        {
+            Assert.AreEqual(true, Number.NumbersTest(x), "false" + x);
+        }
+        
+        [TestCase(5)]
+        [TestCase(27)]
+        [TestCase(400)]
+        [TestCase(4586)]
+        public void IsntPerfect(int x)
+        {
+            Assert.AreEqual(false, Number.NumbersTest(x), "false" + x);
+        }
     }
 }
+
